a45954853c5b80d12c603fc027da8e21d723ebee
papluxd <99307146+papluxd@users.noreply.github.com>
2023-01-12 12:32:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,7 +50,7 @@ async def start(client, message):
                     InlineKeyboardButton('Help ⚡', callback_data='help'),
                     InlineKeyboardButton('About Me 🙈', callback_data='about')
                 ],[
-                    InlineKeyboardButton('Backup Channel ✅',url='https://t.me/+nOQfyrzXjCBkMzk9')
+                    InlineKeyboardButton('Backup Channel ✅',url='https://t.me/+LSA9HRGlQ8dkNDll')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
