b58e8c505e853a5bd81351bf40baa3924d98f166
Ajaykum01 <115863768+Ajaykum01@users.noreply.github.com>
2023-07-08 22:02:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,7 +60,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>If You Want Any Movie, Series Please Join Our Request Groups \n \n 🏆 Request Group: [Click Here 🎗](https://t.me/Moviesss4ers)</b>")
+    await message.reply_text("<b>If You Want Any Movie, Series Please Join Our Request Groups \n \n 🏆 Request Group: [Click Here 🎗](https://t.me/leothalapathy67vijay)</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
