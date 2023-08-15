2f23ae434a1ac9ee8bc1f792ae44bdb8450ebb91
Ajaykum01 <115863768+Ajaykum01@users.noreply.github.com>
2023-08-15 18:44:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -147,7 +147,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                          [
-                          InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://t.me/moonxlinks')
+                          InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://telegram.me/+pihWAHWUSMFkZmNl')
                          ]
                         ]
                     )
@@ -163,7 +163,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                          [
-                          InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://t.me/moonxlinks')
+                          InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://telegram.me/+pihWAHWUSMFkZmNl')
                          ]
                         ]
                     )
@@ -231,7 +231,7 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                      InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://t.me/moonxlinks')
+                      InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://telegram.me/+pihWAHWUSMFkZmNl')
                      ]
                     ]
                 )
@@ -271,7 +271,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
              [
-              InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://t.me/moonxlinks')
+              InlineKeyboardButton('𝐉𝐨𝐢𝐧 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 🎬', url='https://telegram.me/+pihWAHWUSMFkZmNl')
              ]
             ]
         )
