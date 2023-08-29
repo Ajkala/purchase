cfa96f2c0c3a60cf245ac554d16a8d6c2b9974c1
Ajkala <119678149+Ajkala@users.noreply.github.com>
2023-08-29 19:45:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Ajaykum01/purchase /RolexTG 
+  git clone https://github.com/Ajkala/purchase /RolexTG 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /RolexTG
