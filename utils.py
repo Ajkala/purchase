a1a74225491f059162f233b18b8594380318342d
Ajkala <119678149+Ajkala@users.noreply.github.com>
2023-09-11 19:05:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://urltamilann.blogspot.com/api'
+    url = f'https://1tamilblasterss.blogspot.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
